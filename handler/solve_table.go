6ae0efad454c223bda502178f4d08891fee1af0b
madsnot <dashenka.proskuryakova@gmail.com>
2023-01-23 21:34:58 +0300

    modified table solving and update README.md

--- handler/solve_table.go
+++ handler/solve_table.go
@@ -9,60 +9,96 @@ import (
 
 func SolveTable(table map[string]string, eq []string) error {
 	var (
-		res, arg1, arg2 int
-		op              string
-		expr            []string
-		err             error
+		res, arg1, arg2    int
+		op                 string
+		expr               []string
+		err                error
+		statusTableSolving bool
 	)
 	errMsg := []string{"Invalid expression. ",
 		"Unknown %s cell value.",
-		"Division by zero in: %s."}
+		"Division by zero in: %s.",
+		"Invalid table structure."}
 
-	for _, cell := range eq {
-		expr, err = parsers.ParserStrToExpr(table[cell])
-		if err != nil {
-			return err
-		}
+	lenEq := len(eq)
+	for lenEq > 0 {
 
-		if !validator.ValidateNum(expr[0]) {
-			arg1, err = strconv.Atoi(table[expr[0]])
-		} else {
-			arg1, err = strconv.Atoi(expr[0])
-		}
-		if err != nil {
-			return fmt.Errorf(errMsg[0]+errMsg[1], expr[0])
-		}
+		statusTableSolving = false
 
-		if len(expr) == 3 {
-			if !validator.ValidateNum(expr[2]) {
-				arg2, err = strconv.Atoi(table[expr[2]])
+		for ind := 0; ind < lenEq; ind++ {
+			cell := eq[ind]
+
+			expr, err = parsers.ParserStrToExpr(table[cell])
+			if err != nil {
+				return err
+			}
+			if !validator.ValidateNum(expr[0]) {
+				str := []rune(table[expr[0]])
+				if len(str) == 0 {
+					return fmt.Errorf(errMsg[0]+errMsg[1], expr[0])
+				}
+				if str[0] == '=' {
+					continue
+				}
+				arg1, err = strconv.Atoi(string(str))
 			} else {
-				arg2, err = strconv.Atoi(expr[2])
+				arg1, err = strconv.Atoi(expr[0])
 			}
 			if err != nil {
-				return fmt.Errorf(errMsg[0]+errMsg[1], expr[2])
+				return fmt.Errorf(errMsg[0]+errMsg[1], expr[0])
 			}
-			op = expr[1]
+
+			if len(expr) == 3 {
+				if !validator.ValidateNum(expr[2]) {
+					str := []rune(table[expr[2]])
+					if len(str) == 0 {
+						return fmt.Errorf(errMsg[0]+errMsg[1], expr[2])
+					}
+					if str[0] == '=' {
+						continue
+					}
+					arg2, err = strconv.Atoi(string(str))
+				} else {
+					arg2, err = strconv.Atoi(expr[2])
+				}
+				if err != nil {
+					return fmt.Errorf(errMsg[0]+errMsg[1], expr[2])
+				}
+				op = expr[1]
+			}
+
+			switch op {
+			case "+":
+				res = arg1 + arg2
+			case "-":
+				res = arg1 - arg2
+			case "*":
+				res = arg1 * arg2
+			case "/":
+				if arg2 == 0 {
+					return fmt.Errorf(errMsg[0]+errMsg[2], cell)
+				} else {
+					res = arg1 / arg2
+				}
+			default:
+				res = arg1
+			}
+
+			table[cell] = strconv.Itoa(res)
+
+			if lenEq > 1 {
+				eq[ind] = eq[lenEq-1]
+				eq[lenEq-1] = ""
+				eq = eq[:lenEq-1]
+			}
+			statusTableSolving = true
+			arg1, arg2, op = 0, 0, ""
+			lenEq--
 		}
 
-		switch op {
-		case "+":
-			res = arg1 + arg2
-		case "-":
-			res = arg1 - arg2
-		case "*":
-			res = arg1 * arg2
-		case "/":
-			if arg2 == 0 {
-				return fmt.Errorf(errMsg[0]+errMsg[2], cell)
-			} else {
-				res = arg1 / arg2
-			}
-		default:
-			res = arg1
+		if !statusTableSolving {
+			return fmt.Errorf(errMsg[3])
 		}
-
-		table[cell] = strconv.Itoa(res)
 	}
 
 	return nil
